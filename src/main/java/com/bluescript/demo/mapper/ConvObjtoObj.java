3215420a5e03c608ee14e4168e5b64a6226f1fd9
edward7904448427 <quicksilver7m@gmail.com>
2021-11-29 20:04:43 +0530

    Added Object copy codes

--- src/main/java/com/bluescript/demo/mapper/ConvObjtoObj.java
+++ src/main/java/com/bluescript/demo/mapper/ConvObjtoObj.java
@@ -42,25 +42,32 @@ public interface ConvObjtoObj {
     // CaPolicyCommon db2CommonToCaPolicyCommon(IgetHousePolicyJpaDto policy);
 //--- completed
 
-    @Mapping(target = "caEWithProfits", source = "db2HPropertytype")
-    @Mapping(target = "caEEquities", source = "db2HBedrooms")
-    @Mapping(target = "caEManagedFund", source = "db2HValue")
-    @Mapping(target = "caEFundName", source = "db2HHousename")
-    @Mapping(target = "caETerm", source = "db2HHousenumber")
-    @Mapping(target = "caESumAssured", source = "db2HPostcode")
-   
+    @Mapping(target = "caHPropertyType", source = "db2HPropertytype")
+    @Mapping(target = "caHBedrooms", source = "db2HBedrooms")
+    @Mapping(target = "caHValue", source = "db2HValue")
+    @Mapping(target = "caHHouseName", source = "db2HHousename")
+    @Mapping(target = "caHHouseNumber", source = "db2HHousenumber")
+    @Mapping(target = "caHPostcode", source = "db2HPostcode")
     CaHouse db2HouseToCaHouse(IgetHousePolicyJpaDto houseDto);
 
+    
+    @Mapping(target = "caMMake", source = "db2MMake")
+    @Mapping(target = "caMModel", source = "db2MModel")
+    @Mapping(target = "caMValue", source = "db2MValue")
+    @Mapping(target = "caMRegnumber", source = "db2MRegnumber")
+    @Mapping(target = "caMColour", source = "db2MColour")
+    @Mapping(target = "caMCc", source = "db2MCc")
+    @Mapping(target = "caMManufactured", source = "db2MManufactured")
+    @Mapping(target = "caMPremium", source = "db2MPremium")
+    @Mapping(target = "caMAccidents", source = "db2MAccidents")
+    CaMotor db2MotorToCaMotor(IgetMotorPolicyJpaDto motorDto);
 
-
-    @Mapping(target = "caEWithProfits", source = "db2HPropertytype")
-    @Mapping(target = "caEEquities", source = "db2HBedrooms")
-    @Mapping(target = "caEManagedFund", source = "db2HValue")
-    @Mapping(target = "caEFundName", source = "db2HHousename")
-    @Mapping(target = "caETerm", source = "db2HHousenumber")
-    @Mapping(target = "caESumAssured", source = "db2HPostcode")
-    @Mapping(target = "caESumAssured", source = "db2HPostcode")
-    @Mapping(target = "caESumAssured", source = "db2HPostcode")
-    CaHouse db2MotorToCaMotor(IgetHousePolicyJpaDto houseDto);
+    @Mapping(target = "caCNum", source = "db2CNum")      
+    @Mapping(target = "caCDate", source = "db2CDate")
+    @Mapping(target = "caCPaid", source = "db2CPaid")
+    @Mapping(target = "caCValue", source = "db2CValue")
+    @Mapping(target = "caCCause", source = "db2CCause")
+    @Mapping(target = "caCObservations", source = "db2CObservations")
+    CaComm db2commToCacomm(IgetCommJpaDto motorDto);
 
 }
